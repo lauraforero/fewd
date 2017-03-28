5090c44738e0e8716520340565eeda9854f1f8dd
lauraforero <laura.forero@whiteops.com>
2017-03-27 22:57:44 -0400

    homework day 11

--- d11/materials/jquery-city/js/app.js
+++ d11/materials/jquery-city/js/app.js
@@ -1,2 +1,22 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
+
+$('.thumb').click(function(event){
+  
+  var imageId = event.target.id;
+  
+  if (imageId == 'first'){
+    $('#target-image').attr('src', 'img/1.jpg');
+      
+  } else if (imageId== 'second'){
+    $('#target-image').attr('src', 'img/2.jpg');
+
+  } else if (imageId == 'third'){
+    $('#target-image').attr('src', 'img/3.jpg');
+
+  } else {
+    $('#target-image').attr('src', 'img/4.jpg');
+  }
+
+});
+
