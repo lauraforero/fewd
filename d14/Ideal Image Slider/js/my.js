a82a45044a5bfaed96d25611d8be6c2cc8ebe6b9
lauraforero <laura.forero@whiteops.com>
2017-04-04 21:27:00 -0400

    work day 14

---
+++ d14/Ideal Image Slider/js/my.js
@@ -0,0 +1,6 @@
+var slider = new IdealImageSlider.Slider({
+  selector: '#slider',
+  height: 600, // Required but can be set by CSS
+  interval: 1000,
+
+});
---
+++ d14/owl/js/my.js
@@ -0,0 +1,8 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  items: 1,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 1000,
+
+});
