b28183862ab1b87728ecb43015f2f215a28c92cc
lauraforero <laura.forero@whiteops.com>
2017-04-11 18:32:52 -0400

    day15

---
+++ d15/solutions/weather/js/app.js
@@ -0,0 +1,20 @@
+
+$("button").on("click", function(){
+  
+var city = $("input").val();
+console.log(city);
+
+//var temperature= $("span.temperature").val();
+
+
+  
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=london,uk&units=imperial&appid=2ace662e181792f06072837e47646755",function(data){
+    console.log(data);
+  });
+ 
+ //$("span.temperature").text(main["temp"]);
+
+
+
+});
+
